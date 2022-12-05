e7cfa9c59c44d0283d2e287de37599991baedec4
ganesh93769 <ganeshreddy256@gmail.com>
2022-12-05 14:26:18 +0530

    new commit

---
+++ SampleRestapi_1/SampleRestapi_1/src/main/java/SampleRestapi/SampleRestapi_1/SampleRestapi1Application.java
@@ -0,0 +1,13 @@
+package SampleRestapi.SampleRestapi_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestapi1Application.class, args);
+	}
+
+}
---
+++ SampleRestapi_1/SampleRestapi_1/src/test/java/SampleRestapi/SampleRestapi_1/SampleRestapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package SampleRestapi.SampleRestapi_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
